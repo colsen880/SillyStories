82173fad29376e1966365d786e821e3f1efd1dad
Chad Olsen <colsen880@gmail.com>
2024-02-26 11:59:11 -0700

    Initial Commit

---
+++ SillyStories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SillyStories
+//
+//  Created by Chad Olsen on 2/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SillyStories/SillyStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SillyStoriesApp.swift
+//  SillyStories
+//
+//  Created by Chad Olsen on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SillyStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
